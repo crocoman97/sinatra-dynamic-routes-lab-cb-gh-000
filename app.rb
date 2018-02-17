a94a11c90d78862eb58932a6f889337e63918fd1
Rauof M <abdelrauof197@gmail.com>
2018-02-17 11:20:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/reversename/:name' do 
+    erb :reverse
+  end
+  
+  get '/square/:number' do 
+    erb :square
+  end
+  
+  get '/say/:number/:phrase' do 
+    erb :say
+  end
 
 end
